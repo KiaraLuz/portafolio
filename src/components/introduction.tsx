15127d2360b47d0643958f368b5833f533bbb8fc
KiaraLuz <fernandezperezkiara@gmail.com>
2025-02-24 11:19:50 -0500

    feat: :sparkles: state
    state work

--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -7,15 +7,13 @@ export default function Introduction() {
   return (
     <div className="flex max-h-screen flex-col items-center justify-between pt-10 sm:flex-row">
       <div className="mb-8 flex flex-col justify-center gap-4 p-0 sm:m-0 sm:py-8 sm:pr-8">
-        <Image
-          className="rounded-full"
-          src="/perfil.webp"
-          alt="Perfil de Kiara"
-          width={80}
-          height={80}
-        />
+        <div>
+          <span className="rounded-full bg-accent/50 px-3 py-2 text-sm font-light text-foreground">
+            Lista para trabajar
+          </span>
+        </div>
         <h1 className="text-4xl font-bold sm:text-5xl">Hola, soy Kiara</h1>
-        <p className="mt-4 max-w-lg text-base sm:text-lg lg:text-xl">
+        <p className="max-w-lg text-base sm:text-lg lg:text-xl">
           Soy <strong className="text-accent">frontend developer</strong> con
           interés en adquirir experiencia laboral y seguir aprendiendo para
           aportar valor a proyectos digitales.
