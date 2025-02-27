dfa174c3e33b5ca4daf594b1578060da171a76da
KiaraLuz <fernandezperezkiara@gmail.com>
2025-02-27 17:26:26 -0500

    feat: :sparkles: sobre mi
    es igual

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,6 +1,16 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Image from "next/image";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import Link from "next/link";
+import { Badge } from "./ui/badge";
 
 export default function About() {
   return (
@@ -13,15 +23,80 @@ export default function About() {
       <h2 id="sobre-mi" className="scroll-mt-20 text-4xl font-bold">
         Sobre Mí
       </h2>
-      <p className="text-base">
-        Me llamo Kiara, soy desarrolladora frontend con interés en adquirir
-        experiencia laboral y seguir aprendiendo para aportar valor a proyectos
-        digitales.
-      </p>
-      <p>
-        Estudio en la Universidad Nacional Federico Villarreal, cursando
-        actualmente el 8vo ciclo de la carrera de Ingeniería de Sistemas
-      </p>
+      <Card className="flex flex-col gap-4 p-4 md:flex-row">
+        <div className="flex min-w-[300px] items-center justify-center overflow-hidden rounded-lg md:w-auto">
+          <Image
+            alt="Kiara"
+            src="/perfil.webp"
+            className="z-0 h-full w-full rounded-lg object-cover transition-all group-hover:scale-105"
+            width={400}
+            height={400}
+          />
+        </div>
+        <div className="flex w-full flex-col gap-4">
+          <CardHeader className="gap-2 p-0">
+            <CardTitle>
+              <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2">
+                  Desarrolladora Frontend
+                  <Link
+                    href="https://github.com/kiaraluz"
+                    target="_blank"
+                    aria-label="link-github"
+                  >
+                    <Image
+                      alt="GitHub"
+                      src="/svgs/github.svg"
+                      width={24}
+                      height={24}
+                    />
+                  </Link>
+                </div>
+              </div>
+            </CardTitle>
+            <CardDescription className="flex flex-col gap-2">
+              <p className="text-base leading-relaxed">
+                Soy <span className="font-semibold">Kiara</span>, desarrolladora
+                frontend con experiencia en la creación de interfaces dinámicas,
+                optimizadas y accesibles. Actualmente, estudio el{" "}
+                <span className="text-secondary-foreground">9no ciclo</span> de
+                <span className="text-secondary-foreground">
+                  {" "}
+                  Ingeniería de Sistemas
+                </span>{" "}
+                en la
+                <span className="text-secondary-foreground">
+                  {" "}
+                  Universidad Nacional Federico Villarreal
+                </span>
+                .
+              </p>
+              <p className="text-base leading-relaxed">
+                Me especializo en el desarrollo web, asegurando que las
+                aplicaciones sean eficientes y fáciles de usar. He trabajado en
+                proyectos aplicando metodologías ágiles y siempre busco mejorar
+                mis habilidades y asumir nuevos retos.
+              </p>
+              <p className="text-base leading-relaxed">
+                También me interesa el diseño UX/UI y la optimización de
+                rendimiento en el frontend. En mi tiempo libre, disfruto de los
+                videojuegos y explorar nuevas tecnologías.
+              </p>
+            </CardDescription>
+          </CardHeader>
+          <CardFooter className="flex flex-wrap gap-2 p-0">
+            <Badge variant="secondary">
+              Estudiante de Ingeniería de Sistemas
+            </Badge>
+            <Badge variant="secondary">Apasionada por la tecnología</Badge>
+            <Badge variant="secondary">Autodidacta</Badge>
+            <Badge variant="secondary">
+              En búsqueda de nuevas oportunidades
+            </Badge>
+            <Badge variant="secondary">Amante de los videojuegos</Badge>
+          </CardFooter>
+        </div>
+      </Card>
     </motion.section>
   );
 }
